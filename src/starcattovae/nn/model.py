f21a591f116497dd6d2207cdf5528533452fd768
Tarin Eccleston <tarin.eccleston@gmail.com>
2024-11-11 22:03:45 +1300

    implement rudimentary encoder/decoder/model classes

---
+++ src/starcattovae/nn/model.py
@@ -0,0 +1,24 @@
+import torch
+from torch import nn
+
+from src.starcattovae.nn.encoder import Encoder
+from src.starcattovae.nn.decoder import Decoder
+
+class Model(nn.Module):
+    def __init__(self, Encoder, Decoder):
+        super(Model, self).__init__()
+        self.Encoder = Encoder
+        self.Decoder = Decoder
+        
+    def reparameterization(self, mean, var):
+        epsilon = torch.randn_like(var).to(DEVICE)        # sampling epsilon        
+        z = mean + var*epsilon                          # reparameterization trick
+        return z
+        
+                
+    def forward(self, x):
+        mean, log_var = self.Encoder(x)
+        z = self.reparameterization(mean, torch.exp(0.5 * log_var)) # takes exponential function (log var -> var)
+        x_hat            = self.Decoder(z)
+        
+        return x_hat, mean, log_var
